fe585c526e1912b5f869a8fd009089951cd03479
Jeff Cross <middlefloor@gmail.com>
2019-08-26 22:16:09 -0700

    Configure Google Sheets upload

--- pwmetrics-config.js
+++ pwmetrics-config.js
@@ -1,3 +1,23 @@
+require('dotenv').config();
+
 module.exports = {
+  flags: {
+    submit: true
+  },
+  sheets: {
+    type: 'GOOGLE_SHEETS',
+    options: {
+      /** Replace with your own spreadsheet id. */
+      spreadsheetId: '13C-qAV-j2TyaQ8ahvc4sKCsBHCyu-P4s3NntPd-xO9M',
+      tableName: 'data',
+      uploadMedian: true
+    }
+  },
+  /**
+   * To get client secret:
+   * 1. Go to this URL and follow step 1: https://developers.google.com/sheets/api/quickstart/nodejs#step_1_turn_on_the_api_name
+   * 2. Move the credentials JSON file to the root of the workspace
+   */
+  clientSecret: require('./credentials.json'),
   url: 'http://localhost:4200'
 };
