c4a6d684478ceb7e9fda9f4e576d4291ba3cc09a
Jeffrey Cross <middlefloor@gmail.com>
2019-10-21 18:14:01 -0500

    WIP: Add basic styling to header and secure page

--- libs/feat-secure-page/src/lib/feat-secure-page.module.ts
+++ libs/feat-secure-page/src/lib/feat-secure-page.module.ts
@@ -5,6 +5,20 @@ import { RouterModule } from '@angular/router';
 @Component({
   template: `
     <h1>Welcome to the Secure Page</h1>
+    <a
+      data-flickr-embed="true"
+      href="https://www.flickr.com/photos/whiteoctevents/48803222807/in/album-72157711083363381/"
+      title="AngularConnect 2019"
+      ><img
+        src="https://live.staticflickr.com/65535/48803222807_25ccd7d907_b.jpg"
+        width="1024"
+        height="683"
+        alt="AngularConnect 2019"/></a
+    ><script
+      async
+      src="//embedr.flickr.com/assets/client-code.js"
+      charset="utf-8"
+    ></script>
   `
 })
 export class SecurePage {}
