ab8df08ca005e6be9e7c67cb2c6b6b91c9c8effb
Jeffrey Cross <middlefloor@gmail.com>
2019-10-21 19:05:09 -0500

    WIP: Puppeteer is working with pwmetrics

--- tools/puppeteer-login.js
+++ tools/puppeteer-login.js
@@ -1,19 +1,41 @@
 const puppeteer = require('puppeteer');
-
+const PWMetrics = require('pwmetrics');
+const defaultOpts = require('../pwmetrics-config');
 const PUPPET_PORT = 3400;
+const options = {
+  ...defaultOpts,
+  sheets: {
+    ...defaultOpts.sheets,
+    options: {
+      ...defaultOpts.sheets.options,
+      tableName: 'secure-page-data'
+    }
+  },
+  flags: {
+    ...defaultOpts.flags,
+    port: PUPPET_PORT,
+    disableStorageReset: true
+  }
+};
+
 puppeteer
   .launch({
-    args: [`--remote-debugging-port=${PUPPET_PORT}`]
+    args: [`--remote-debugging-port=${PUPPET_PORT}`],
+    headless: false
   })
   .then(async browser => {
     const page = await browser.newPage();
     await page.goto('http://localhost:4200/auth/login');
-    const emailInput = await page.$('input[type="email"]');
+    const email = await page.$('input[type="email"]');
     // The actual credentials should be stored in a .env file or similar in CI environment.
-    await emailInput.type('jeff@nrwl.io');
-    const passwordInput = await page.$('input[type="password"]');
-    await passwordInput.type('password');
+    await email.type('jeff@nrwl.io');
+    const password = await page.$('input[type="password"]');
+    await password.type('password');
     const submitButton = await page.$('button');
     submitButton.click();
     await page.waitForNavigation();
-  });
+    await page.close();
+    const pwMetrics = new PWMetrics('http://localhost:4200/secure', options);
+    await pwMetrics.start();
+  })
+  .then(() => process.exit(0), () => process.exit(1));
